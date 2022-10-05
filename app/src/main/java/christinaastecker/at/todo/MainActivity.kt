300cf46e2ecf74eb0a25be4a46deba1dce13a095
Christina Astecker <2010653051@stud.fh-kufstein.ac.at>
2022-10-05 18:20:24 +0200

    first commit

---
+++ app/src/main/java/christinaastecker/at/todo/MainActivity.kt
@@ -0,0 +1,11 @@
+package christinaastecker.at.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
